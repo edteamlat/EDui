cc4ae81bc93e9a7c327cd610d6a5288ac02126a6
alejogs4 <alejogs4@gmail.com>
2019-07-05 21:26:45 -0500

    Agrega botones tiny y botones micro

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,56 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import PropTypes from 'prop-types';
+import { colors } from '../colors';
+import { sizes } from '../font-sizes';
 
-export const Button = styled.button`
+const Button = styled.button`
   display          : inline-block;
   padding          : .5rem 1rem;
-  background-color : #007BDF;
+  background-color : ${({ color }) => colors[color] || colors['first-color']};
   color            : #FFF;
   cursor           : pointer;
   text-decoration  : none;
   line-height      : inherit;
   border-radius    : .25rem;
   transition       : transform .05s;
-  border           : 1px solid #007BDF;
+  border           : 1px solid ${({ color }) => colors[color] || colors['first-color']};
   text-align       : center;
   user-select      : none;
+
+	:hover {
+		background-color :   ${({ color }) => colors[`${color}-alt`] || colors[color] || colors['first-color']};
+    text-decoration  : inherit;
+	}
+
+	:active {
+		transform : scale(.99);
+	}
+
+	/* Tiny buttons */
+	${props => props.tiny && css`
+		padding   : .25rem .5rem;
+  	font-size : ${sizes.small};
+	`}
+
+	/* Micro buttons */
+	${props => props.micro && css`
+		padding: 0 .25rem;
+  	font-size: ${sizes.small};
+  	line-height: 1.3;
+  	border-radius: 0;
+	`}
 `;
+
+Button.propTypes = {
+	color: PropTypes.string,
+	tiny: PropTypes.bool,
+	micro: PropTypes.bool
+};
+
+Button.propTypes = {
+	color: 'first-color',
+	tiny: false,
+	micro: false
+};
+
+export { Button };
--- stories/Button.js
+++ stories/Button.js
@@ -1,11 +1,41 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable no-tabs */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from '../src';
 
 storiesOf('Button', module)
-	.add('With text', () => (
-		<Button>Test button for EDui</Button>
-	))
-	.add('With emojis', () => (
-		<Button>😀 😎 👍 💯</Button>
-	))
+  .add('All buttons', () => (
+  <>
+    <Button>Test button for EDui</Button>
+    <Button color="first-color">Main color</Button>
+    <Button color="accent-color">Accent color</Button>
+    <Button color="second-color">Second color</Button>
+    <Button color="third-color">Third color</Button>
+    <Button color="dark-color">Dark color</Button>
+    <Button color="unknown-color">Unknown color</Button>
+  </>
+  ))
+  .add('Tiny buttons', () => (
+  <>
+    <Button tiny>Test button for EDui</Button>
+    <Button color="first-color" tiny>Main color</Button>
+    <Button color="accent-color" tiny>Accent color</Button>
+    <Button color="second-color" tiny>Second color</Button>
+    <Button color="third-color" tiny>Third color</Button>
+    <Button color="dark-color" tiny>Dark color</Button>
+    <Button color="unknown-color" tiny>Unknown color</Button>
+  </>
+  ))
+  .add('Micro buttons', () => (
+  <>
+    <Button micro>Test button for EDui</Button>
+    <Button color="first-color" micro>Main color</Button>
+    <Button color="accent-color" micro>Accent color</Button>
+    <Button color="second-color" micro>Second color</Button>
+    <Button color="third-color" micro>Third color</Button>
+    <Button color="dark-color" micro>Dark color</Button>
+    <Button color="unknown-color" micro>Unknown color</Button>
+  </>
+  ));
