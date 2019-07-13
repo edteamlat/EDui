63646072bab4d71575fc2c146cf1fa16b2f19305
alejogs4 <alejogs4@gmail.com>
2019-07-12 21:11:16 -0500

    Agrega botones full a componente Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,56 +1,76 @@
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
-import { colors } from '../colors';
 import { sizes } from '../font-sizes';
+import { getColor, getColorWithAlt } from '../utils/colors-helpers';
 
 const Button = styled.button`
   display          : inline-block;
   padding          : .5rem 1rem;
-  background-color : ${({ color }) => colors[color] || colors['first-color']};
+  background-color : ${getColor};
   color            : #FFF;
   cursor           : pointer;
   text-decoration  : none;
   line-height      : inherit;
   border-radius    : .25rem;
   transition       : transform .05s;
-  border           : 1px solid ${({ color }) => colors[color] || colors['first-color']};
+  border           : 1px solid ${getColor};
   text-align       : center;
   user-select      : none;
 
-	:hover {
-		background-color :   ${({ color }) => colors[`${color}-alt`] || colors[color] || colors['first-color']};
+  :hover {
+    background-color :   ${getColorWithAlt};
     text-decoration  : inherit;
-	}
+  }
 
-	:active {
-		transform : scale(.99);
-	}
+  :active {
+    transform : scale(.99);
+  }
 
-	/* Tiny buttons */
-	${props => props.tiny && css`
-		padding   : .25rem .5rem;
-  	font-size : ${sizes.small};
+  /* Tiny buttons */
+  ${props => props.tiny && css`
+    padding   : .25rem .5rem;
+    font-size : ${sizes.small};
+  `}
+
+  /* Micro buttons */
+  ${props => props.micro && css`
+    padding: 0 .25rem;
+    font-size: ${sizes.small};
+    line-height: 1.3;
+    /* border-radius: 0; */
+  `}
+
+	${props => props.ghost && css`
+		color      : ${getColorWithAlt};
+  	background : none;
+  	border     : 1px solid;
+
+  	&:hover {
+    	background   : ${getColorWithAlt};
+    	color        : #FFF;
+    	border-color : transparent;
+  	}
 	`}
 
-	/* Micro buttons */
-	${props => props.micro && css`
-		padding: 0 .25rem;
-  	font-size: ${sizes.small};
-  	line-height: 1.3;
-  	border-radius: 0;
+	${props => props.full && css`
+		margin-right : 0;
+  	margin-left  : 0;
+  	width        : 100%;
 	`}
 `;
 
 Button.propTypes = {
 	color: PropTypes.string,
 	tiny: PropTypes.bool,
-	micro: PropTypes.bool
+	micro: PropTypes.bool,
+	full: PropTypes.bool
 };
 
 Button.propTypes = {
 	color: 'first-color',
 	tiny: false,
-	micro: false
+	micro: false,
+	full: false
 };
 
 export { Button };
--- stories/Button.js
+++ stories/Button.js
@@ -6,36 +6,58 @@ import { storiesOf } from '@storybook/react';
 import { Button } from '../src';
 
 storiesOf('Button', module)
-  .add('All buttons', () => (
-  <>
-    <Button>Test button for EDui</Button>
-    <Button color="first-color">Main color</Button>
-    <Button color="accent-color">Accent color</Button>
-    <Button color="second-color">Second color</Button>
-    <Button color="third-color">Third color</Button>
-    <Button color="dark-color">Dark color</Button>
-    <Button color="unknown-color">Unknown color</Button>
-  </>
-  ))
-  .add('Tiny buttons', () => (
-  <>
-    <Button tiny>Test button for EDui</Button>
-    <Button color="first-color" tiny>Main color</Button>
-    <Button color="accent-color" tiny>Accent color</Button>
-    <Button color="second-color" tiny>Second color</Button>
-    <Button color="third-color" tiny>Third color</Button>
-    <Button color="dark-color" tiny>Dark color</Button>
-    <Button color="unknown-color" tiny>Unknown color</Button>
-  </>
-  ))
-  .add('Micro buttons', () => (
-  <>
-    <Button micro>Test button for EDui</Button>
-    <Button color="first-color" micro>Main color</Button>
-    <Button color="accent-color" micro>Accent color</Button>
-    <Button color="second-color" micro>Second color</Button>
-    <Button color="third-color" micro>Third color</Button>
-    <Button color="dark-color" micro>Dark color</Button>
-    <Button color="unknown-color" micro>Unknown color</Button>
-  </>
-  ));
+	.add('All buttons', () => (
+		<>
+			<Button>Test button for EDui</Button>
+			<Button color="first-color">Main color</Button>
+			<Button color="accent-color">Accent color</Button>
+			<Button color="second-color">Second color</Button>
+			<Button color="third-color">Third color</Button>
+			<Button color="dark-color">Dark color</Button>
+			<Button color="unknown-color">Unknown color</Button>
+		</>
+	))
+	.add('Tiny buttons', () => (
+		<>
+			<Button tiny>Test button for EDui</Button>
+			<Button color="first-color" tiny>Main color</Button>
+			<Button color="accent-color" tiny>Accent color</Button>
+			<Button color="second-color" tiny>Second color</Button>
+			<Button color="third-color" tiny>Third color</Button>
+			<Button color="dark-color" tiny>Dark color</Button>
+			<Button color="unknown-color" tiny>Unknown color</Button>
+		</>
+	))
+	.add('Micro buttons', () => (
+		<>
+			<Button micro>Test button for EDui</Button>
+			<Button color="first-color" micro>Main color</Button>
+			<Button color="accent-color" micro>Accent color</Button>
+			<Button color="second-color" micro>Second color</Button>
+			<Button color="third-color" micro>Third color</Button>
+			<Button color="dark-color" micro>Dark color</Button>
+			<Button color="unknown-color" micro>Unknown color</Button>
+		</>
+	))
+	.add('Ghost buttons', () => (
+		<>
+			<Button ghost>Test button for EDui</Button>
+			<Button color="first-color" ghost>Main color</Button>
+			<Button color="accent-color" ghost>Accent color</Button>
+			<Button color="second-color" ghost>Second color</Button>
+			<Button color="third-color" ghost>Third color</Button>
+			<Button color="dark-color" ghost>Dark color</Button>
+			<Button color="unknown-color" ghost>Unknown color</Button>
+		</>
+	))
+	.add('Full buttons', () => (
+		<>
+			<Button full>Test button for EDui</Button>
+			<Button color="first-color" full>Main color</Button>
+			<Button color="accent-color" full>Accent color</Button>
+			<Button color="second-color" full>Second color</Button>
+			<Button color="third-color" full>Third color</Button>
+			<Button color="dark-color" full>Dark color</Button>
+			<Button color="unknown-color" full>Unknown color</Button>
+		</>
+	));
