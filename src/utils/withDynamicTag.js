4c6a8996f2c830048595960309c492689e0a3206
alejogs4 <alejogs4@gmail.com>
2019-07-20 14:17:57 -0500

    Agrega HOC para generar styled components con tag dinamico

---
+++ src/utils/withDynamicTag.js
@@ -0,0 +1,28 @@
+import { createElement as h } from 'react';
+import styled from 'styled-components';
+
+export const withDynamicTag = (Component) => {
+  const bucket = Object.create(null);
+
+  const DynamicTag = (props) => {
+    const { tag } = props;
+
+    if (typeof tag !== 'string' || !styled.hasOwnProperty(tag)) {
+      return h(Component, props);
+    }
+
+    if (bucket[tag] === undefined) {
+      bucket[tag] = Component.withComponent(tag);
+    }
+
+    return h(bucket[tag], props);
+  };
+
+  const name = Component.displayName || Component.constructor.name;
+
+  if (name) {
+    DynamicTag.displayName = `DynamicTag(${name})`;
+  }
+
+  return DynamicTag;
+};
