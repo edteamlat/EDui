0496f7a2255a57d48d8ae83921fcc7fa8d7aa3e6
alejogs4 <alejogs4@gmail.com>
2019-09-01 21:23:29 -0500

    Soluciona error en hover del tab

--- src/components/Tabs/Tab.jsx
+++ src/components/Tabs/Tab.jsx
@@ -1,6 +1,14 @@
 import styled , { css } from 'styled-components';
 import PropTypes from 'prop-types';
 
+const activeMixin = css `
+	/* --tab-color variable is defined at Tabs Component */
+	box-shadow: 0 -3px var(--tab-color) inset;
+	color: var(--tab-color);
+	opacity: 1;
+	text-decoration: none;
+`
+
 export const Tab = styled.div`
 	border: none;
 	background: none;
@@ -12,13 +20,10 @@ export const Tab = styled.div`
 	flex-wrap: nowrap;
 	cursor: pointer;
 
-	${props => props.active === props.reference && css`
-		/* --tab-color variable is defined at Tabs Component */
-		box-shadow: 0 -3px var(--tab-color) inset;
-		color: var(--tab-color);
-		opacity: 1;
-		text-decoration: none;
-	`};
+	${props => props.active === props.reference && activeMixin};
+	&:hover {
+		${activeMixin}
+	}
 `;
 
 Tab.propTypes = {
